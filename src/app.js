7bde639d540eb1ea4d6f8788d369226cf1f0cf18
yokuny <felipe.vni@hotmail.com>
2023-04-13 22:57:35 -0300

    everything back to express page

--- src/app.js
+++ src/app.js
@@ -1,29 +1,136 @@
 import express from "express";
 import dotenv from "dotenv";
-import cors from "cors";
-import db from "../script/db.js";
+import dayjs from "dayjs";
+import { MongoClient, ServerApiVersion } from "mongodb";
+import { userValidation } from "../script/joi.js";
+
 const app = express();
 app.use(express.json());
-app.use(cors());
 dotenv.config();
 
-import inactiveUser from "../script/inactiveUser.js";
+const key = process.env.DATABASE_URL;
+const initDB = async () => {
+  const client = new MongoClient(key, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    },
+  });
+  try {
+    await client.connect();
+    const database = await client.db();
+    return database;
+  } catch {
+    console.log("error iniciar db");
+  }
+};
+const db = await initDB();
 
-import postParticipants from "./routes/postParticipants.js";
-import getParticipants from "./routes/getParticipants.js";
-import postMessages from "./routes/postMessages.js";
-import getmessages from "./routes/getMessages.js";
-import postStatus from "./routes/postStatus.js";
-app.post("/participants", postParticipants);
-app.get("/participants", getParticipants);
-app.post("/messages", postMessages);
-app.get("/messages", getmessages);
-app.post("/status", postStatus);
+const inactiveUser = async () => {
+  const tenSecondsAgo = Date.now();
+  try {
+    const afk = await db
+      .collection("participants")
+      .find({
+        lastStatus: { $lt: tenSecondsAgo - 10000 },
+      })
+      .toArray();
+    afk.forEach(async (user) => {
+      const time = dayjs().format("HH:mm:ss");
+      await db.collection("messages").insertOne({
+        to: "Todos",
+        text: "sai da sala...",
+        type: "status",
+        from: user.name,
+        time: time,
+      });
+      await db.collection("participants").deleteOne({ name: user.name });
+    });
+  } catch (err) {
+    console.log({ message: err.message });
+  }
+};
 
-const PORT = parseInt(process.env.PORT) || 5000;
+app.post("/participants", async (req, res) => {
+  const { name } = req.body;
+  const { error } = userValidation.validate({ name });
+  if (error) {
+    return res.status(400).send({ message: error.message });
+  } else {
+    try {
+      const usersDatabase = await db.collection("participants").findOne({ name: name });
+      if (usersDatabase) {
+        return res.status(409).send("User already exists");
+      } else {
+        const time = dayjs().format("HH:mm:ss");
+        await db.collection("participants").insertOne({ name: name, lastStatus: Date.now() });
+        await db
+          .collection("messages")
+          .insertOne({ from: name, to: "Todos", text: "entra na sala...", type: "status", time: time });
+        res.sendStatus(201);
+      }
+    } catch (err) {
+      res.status(500).send({ message: err.message });
+    }
+  }
+});
 
-(async () => {
-  await db();
+app.get("/participants", async (req, res) => {
+  try {
+    const participants = await db.collection("participants").find().toArray();
+    res.status(200).send(participants);
+  } catch (err) {
+    res.status(422).send({ message: err.message });
+  }
+});
+app.post("/messages", async (req, res) => {
+  const { to, text, type } = req.body;
+  const { user } = req.headers;
+  try {
+    const userOnline = await db.collection("participants").findOne({ name: user });
+    const from = userOnline.name;
+    const { error } = messageValidation.validate({ to, text, type, from });
+    if (error) {
+      return res.status(422).send({ message: error.message });
+    } else {
+      const time = dayjs().format("HH:mm:ss");
+      await db.collection("messages").insertOne({ to, text, type, from, time });
+      res.sendStatus(201);
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+});
+app.get("/messages", async (req, res) => {
+  const { user } = req.headers;
+  try {
+    const availableMessages = await db
+      .collection("messages")
+      .find({ $or: [{ to: "Todos" }, { to: user }, { from: user }] })
+      .toArray();
+    res.status(200).send(availableMessages);
+  } catch (err) {
+    res.status(422).send("Internal server error");
+  }
+});
+app.post("/status", async (req, res) => {
+  const { user } = req.headers;
+  const { error } = userValidation.validate({ user });
+  if (error) return res.status(404).send({ message: error.message });
+  try {
+    const userOnline = await db.collection("participants").findOne({ name: user });
+    if (userOnline) {
+      await db.collection("participants").updateOne({ name: user }, { $set: { lastStatus: Date.now() } });
+      res.sendStatus(200);
+    } else {
+      res.sendStatus(400);
+    }
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+});
+
+app.listen(5000, async () => {
   setInterval(inactiveUser, 1500);
-  app.listen(PORT);
-})();
+});
