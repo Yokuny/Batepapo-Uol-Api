5d03affb39a7adfba25d32597fadc5b716a8bfc9
yokuny <felipe.vni@hotmail.com>
2023-04-15 16:35:20 -0300

    sanitize

--- src/routes/postMessages.js
+++ src/routes/postMessages.js
@@ -1,17 +1,31 @@
 import dayjs from "dayjs";
+import { stripHtml } from "string-strip-html";
 import { messageValidation } from "../scripts/validation.js";
+
+const sanitize = (dirtyTo, dirtyText, dirtyType, dirtyFrom) => {
+  const time = dayjs().format("HH:mm:ss");
+  const to = stripHtml(dirtyTo).result.trim();
+  const text = stripHtml(dirtyText).result.trim();
+  const type = stripHtml(dirtyType).result.trim();
+  const from = stripHtml(dirtyFrom).result.trim();
+  return { to, text, type, from, time };
+};
 
 const postMessages = async (req, res, db) => {
   const { to, text, type } = req.body;
-  const userName = req.headers.user || req.headers.User;
+  const name = req.headers.user || req.headers.User;
+  const userName = stripHtml(name).result.trim();
 
   try {
     const userOnline = await db.collection("participants").findOne({ name: userName });
     const from = userOnline.name;
+
     const { error } = messageValidation.validate({ to, text, type, from });
     if (error) return res.status(422).send({ message: error.message });
-    const time = dayjs().format("HH:mm:ss");
-    await db.collection("messages").insertOne({ to, text, type, from, time });
+
+    const message = sanitize(to, text, type, from);
+
+    await db.collection("messages").insertOne(message);
     res.sendStatus(201);
   } catch (err) {
     return res.status(422).json({ message: err.message });
--- src/routes/postParticipants.js
+++ src/routes/postParticipants.js
@@ -1,4 +1,5 @@
 import dayjs from "dayjs";
+import { stripHtml } from "string-strip-html";
 import { userValidation } from "../scripts/validation.js";
 
 const postParticipants = async (req, res, db) => {
@@ -7,16 +8,18 @@ const postParticipants = async (req, res, db) => {
   const { error } = userValidation.validate({ name });
   if (error) return res.status(422).send({ message: error.message });
 
+  const userName = stripHtml(name).result.trim();
+
   try {
-    const usersDatabase = await db.collection("participants").findOne({ name: name });
+    const usersDatabase = await db.collection("participants").findOne({ name: userName });
     if (usersDatabase) {
       return res.status(409).send("User already exists");
     } else {
       const time = dayjs().format("HH:mm:ss");
-      await db.collection("participants").insertOne({ name: name, lastStatus: Date.now() });
+      await db.collection("participants").insertOne({ name: userName, lastStatus: Date.now() });
       await db
         .collection("messages")
-        .insertOne({ from: name, to: "Todos", text: "entra na sala...", type: "status", time: time });
+        .insertOne({ from: userName, to: "Todos", text: "entra na sala...", type: "status", time: time });
       res.sendStatus(201);
     }
   } catch (err) {
